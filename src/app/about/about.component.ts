2b6f991b9bb8fd0f682135f3c8a3e90770706be9
Laure <dominguezlaure@gmail.com>
2024-07-04 15:46:40 +0200

    nav links works

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.sass'
+  styleUrl: './about.component.sass',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.sass'
+  styleUrl: './contact.component.sass',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
