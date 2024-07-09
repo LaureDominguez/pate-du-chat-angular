7815d227f1b109ed6f7ad3e6965f93e28dab8831
Laure <dominguezlaure@gmail.com>
2024-07-09 11:02:49 +0200

    added products component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ShopComponent } from './shop/shop.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, //Home
-    { path: 'about', component: AboutComponent }, //About
-    { path: 'shop', component: ShopComponent }, //Shop
-    { path: 'contact', component: ContactComponent}, //Contact
-    { path: '**', redirectTo: '', pathMatch: 'full' } //Error
+  { path: '', title: 'Accueil', component: HomeComponent }, //Home
+  { path: 'about', title: "L'Atelier", component: AboutComponent }, //About
+  { path: 'products', title: 'Les Produits', component: ProductsComponent}, //Products
+  { path: 'shop', title: 'La Boutique', component: ShopComponent }, //Shop
+  { path: 'contact', title: 'Contact', component: ContactComponent }, //Contact
+  { path: '**', redirectTo: '', pathMatch: 'full' }, //Error
 ];
