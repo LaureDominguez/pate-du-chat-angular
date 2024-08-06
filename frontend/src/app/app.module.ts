8a22806d313792358c11442eb3cd77c70c4048d2
Laure <dominguezlaure@gmail.com>
2024-08-06 11:54:00 +0200

    main style done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
-import { ProductService } from './services/product.service';
+// import { ProductService } from './services/product.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent,
+    // ProductsComponent
+  ],
   imports: [BrowserModule],
   providers: [
     provideHttpClient(),
