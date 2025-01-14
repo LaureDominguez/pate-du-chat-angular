6bb2b36aa5d8e61760791b66d8dd3f59ef7f833a
Laure Dominguez <dominguezlaure@gmail.com>
2025-01-14 17:06:48 +0100

    wip on product form, ingredient selection almost done

--- frontend/src/app/components/admin/product-admin/product-form/product-form.component.ts
+++ frontend/src/app/components/admin/product-admin/product-form/product-form.component.ts
@@ -1,12 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { map, Observable, startWith } from 'rxjs';
+import { distinctUntilChanged, map, Observable, startWith } from 'rxjs';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { Ingredient } from '../../ingredient-admin/ingredient-form/ingredient-form.component';
 import { AdminModule } from '../../admin.module';
-
 
 
 @Component({
@@ -19,35 +18,21 @@ import { AdminModule } from '../../admin.module';
 export class ProductFormComponent implements OnInit {
   productForm!: FormGroup;
   ingredientCtrl = new FormControl();
-  filteredIngredients: Observable<Ingredient[]>;
 
   categories: any[] = [];
   composition: Ingredient[] = [];
   ingredients: Ingredient[] = [];
+  filteredIngredients: Observable<Ingredient[]> = new Observable<Ingredient[]>();
+  noResults: boolean = false;
 
-  constructor(
+  constructor (
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<ProductFormComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data: any
-  ) {
-    this.filteredIngredients = this.ingredientCtrl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filteredIngredients(value))
-    );
-  }
-
-  private _filteredIngredients(value: string): Ingredient[] {
-    const filterValue = value.toLowerCase();
-    return this.ingredients.filter((ingredient) =>
-      ingredient.name.toLowerCase().includes(filterValue)
-    );
-  }
+  ) {}
 
   ngOnInit(): void {
-    console.log('admin.component -> ngOnInit -> data : ', this.data);
-
-    // Initialisation des catégories et ingrédients
     this.categories = this.data.categories || [];
     this.ingredients = this.data.ingredients || [];
 
@@ -56,6 +41,12 @@ export class ProductFormComponent implements OnInit {
     if (this.data.product) {
       this.productForm.patchValue({ ...this.data.product });
     }
+    
+    this.filteredIngredients = this.ingredientCtrl.valueChanges.pipe(
+      startWith(''),
+      distinctUntilChanged(),
+      map((value) => this._filteredIngredients(value))
+    );
   }
 
   initForm(): void {
@@ -72,6 +63,42 @@ export class ProductFormComponent implements OnInit {
     });
   }
 
+  private _filteredIngredients(value: string): Ingredient[] {
+    if (!value || value.trim() === '') {
+      this.noResults = false; // Pas de message si le champ est vide
+      return [];
+    }
+    const filterValue = value.toLowerCase().trim();
+
+    const results = this.ingredients.filter((ingredient) =>
+      ingredient.name.toLowerCase().includes(filterValue)
+    );
+    // console.log('results : ', results);
+
+    this.noResults = results.length === 0;
+    // console.log('noResults : ', this.noResults);
+    return results;
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    if (!this.composition.includes(ingredient)) {
+      this.composition.push(ingredient);
+    }
+    this.ingredientCtrl.setValue('', { emitEvent: false });
+  }
+
+  removeIngredient(ingredient: Ingredient): void {
+    const index = this.composition.indexOf(ingredient);
+    if (index >= 0) {
+      this.composition.splice(index, 1);
+    }
+  }
+
+  openIngredientForm() {
+    // Logique pour ouvrir le formulaire ingredient-form
+    console.log('Ouverture du formulaire ingredient-form');
+  }
+
   save(): void {
     if (this.productForm.valid) {
       const productData = { ...this.productForm.value };
@@ -81,19 +108,5 @@ export class ProductFormComponent implements OnInit {
 
   cancel() {
     this.dialogRef.close();
-  }
-
-  addIngredient(ingredient: Ingredient): void {
-    if (!this.composition.includes(ingredient)) {
-      this.composition.push(ingredient);
-    }
-    this.ingredientCtrl.setValue('');
-  }
-
-  removeIngredient(ingredient: Ingredient): void {
-    const index = this.composition.indexOf(ingredient);
-    if (index >= 0) {
-      this.composition.splice(index, 1);
-    }
   }
 }
